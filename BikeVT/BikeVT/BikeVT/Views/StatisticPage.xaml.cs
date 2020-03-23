1abef641801377d36427bae3ed3001724a4743ed
sllarsen <sllarsen@vt.edu>
2020-03-23 15:02:15 -0400

    Adding files

---
+++ BikeVT/BikeVT/BikeVT/Views/StatisticPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BikeVT.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StatisticPage : TabbedPage
+    {
+        public StatisticPage ()
+        {
+            InitializeComponent();
+        }
+    }
+}
