53586c9b12318b4378680a364f73c285dccf038f
Lucas Soohoo <lgsoohoo@vt.edu>
2020-03-25 00:49:57 -0400

    Added code for opening Google Maps tutorial

--- BikeVT/BikeVT/BikeVT/Views/Directions.xaml.cs
+++ BikeVT/BikeVT/BikeVT/Views/Directions.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 using Xamarin.Essentials;
 
 namespace BikeVT.Views
@@ -16,6 +17,24 @@ namespace BikeVT.Views
         public Directions()
         {
             InitializeComponent();
+        }
+        
+        //https://docs.microsoft.com/en-us/xamarin/essentials/maps?context=xamarin%2Fxamarin-forms&tabs=android
+        private async void ButtonOpenCoords_Clicked(object sender, EventArgs e)
+        {
+            if (!double.TryParse(EntryLatitude.Text, out double lat))
+                return;
+            if (!double.TryParse(EntryLongitutde.Text, out double lng))
+                return;
+
+            // Open default "Maps" application
+            await Map.OpenAsync(lat, lng, new MapLaunchOptions
+            {
+                Name = EntryName.Text,
+                NavigationMode = NavigationMode.Bicycling
+                //Automatically sets directions in "Biking" mode
+            });
+
         }
     }
 }
