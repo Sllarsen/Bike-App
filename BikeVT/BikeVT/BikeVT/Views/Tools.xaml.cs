8398df72dbb341f0642834dc7428104b13dbe8d1
sllarsen <sllarsen@vt.edu>
2020-03-25 19:28:55 -0400

    added tools page

---
+++ BikeVT/BikeVT/BikeVT/Views/Tools.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+namespace BikeVT.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tools : ContentPage
+    {
+        // Set speed delay for monitoring changes.
+        SensorSpeed speed = SensorSpeed.UI;
+
+        public Tools()
+        {
+            InitializeComponent();
+            // Register for reading changes.
+            Gyroscope.ReadingChanged += Gyroscope_ReadingChanged;
+        }
+
+
+        void Gyroscope_ReadingChanged(object sender, GyroscopeChangedEventArgs e)
+        {
+            var data = e.Reading;
+            // Process Angular Velocity X, Y, and Z reported in rad/s
+            Console.WriteLine($"Reading: X: {data.AngularVelocity.X}, Y: {data.AngularVelocity.Y}, Z: {data.AngularVelocity.Z}");
+        }
+
+        public void ToggleGyroscope(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Gyroscope.IsMonitoring)
+                    Gyroscope.Stop();
+                else
+                    Gyroscope.Start(speed);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+
+    }
+
+    
+}
