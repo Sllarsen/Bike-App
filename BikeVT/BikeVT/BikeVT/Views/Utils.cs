fcea183abb87f49a056e9fa42f33f08f1dc75c1f
Lucas Soohoo <lgsoohoo@vt.edu>
2020-04-09 13:27:31 -0400

    Ask for location permissions

---
+++ BikeVT/BikeVT/BikeVT/Views/Utils.cs
@@ -0,0 +1,86 @@
+﻿// From https://github.com/jamesmontemagno/PermissionsPlugin/blob/master/samples/PermissionsSample/PermissionsSample/Utils.cs
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BikeVT.Views
+{
+	public static class Utils
+	{
+		public static async Task<PermissionStatus> CheckPermissions(BasePermission permission)
+		{
+			var permissionStatus = await permission.CheckPermissionStatusAsync();
+			bool request = false;
+			if (permissionStatus == PermissionStatus.Denied)
+			{
+				if (Device.RuntimePlatform == Device.iOS)
+				{
+
+					// The following lines have been added
+					// `permission.ToString()` Starts as "Plugin.Permissions.LocationPermission", trim to "LocationPermission"
+					var permissionString = permission.ToString().Split('.')[2];
+					permissionString = permissionString.Substring(0, permissionString.Length - 10);
+					// The above lines have been added
+
+					// The following 4 lines have been edited
+					var title = $"{permissionString} permissions required";
+					var question = $"Please open Settings and allow {permissionString}.";
+					var positive = "Open Settings";
+					var negative = "Deny permission";
+					// The above 4 lines have been edited
+					
+					var task = Application.Current?.MainPage?.DisplayAlert(title, question, positive, negative);
+					if (task == null)
+						return permissionStatus;
+
+					var result = await task;
+					if (result)
+					{
+						CrossPermissions.Current.OpenAppSettings();
+					}
+
+					return permissionStatus;
+				}
+
+				request = true;
+
+			}
+
+			if (request || permissionStatus != PermissionStatus.Granted)
+			{
+				permissionStatus = await permission.RequestPermissionAsync();
+
+
+				if (permissionStatus != PermissionStatus.Granted)
+				{
+					// The following lines have been added
+					// `permission.ToString()` Starts as "Plugin.Permissions.LocationPermission", trim to "LocationPermission"
+					var permissionString = permission.ToString().Split('.')[2];
+					permissionString = permissionString.Substring(0, permissionString.Length - 10);
+					// The above lines have been added
+
+					// The following 4 lines have been edited
+					var title = $"{permissionString} permissions required";
+					var question = $"Please open Settings and allow {permissionString}.";
+					var positive = "Open Settings";
+					var negative = "Deny permission";
+					// The above 4 lines have been edited
+
+					var task = Application.Current?.MainPage?.DisplayAlert(title, question, positive, negative);
+					if (task == null)
+						return permissionStatus;
+
+					var result = await task;
+					if (result)
+					{
+						CrossPermissions.Current.OpenAppSettings();
+					}
+					return permissionStatus;
+				}
+			}
+
+			return permissionStatus;
+		}
+	}
+}
--- BikeVT/BikeVT/BikeVT/Views/WeatherPage.xaml.cs
+++ BikeVT/BikeVT/BikeVT/Views/WeatherPage.xaml.cs
@@ -14,6 +14,10 @@ using Plugin.Permissions.Abstractions;
 using System.Collections.ObjectModel;
 using Plugin.Permissions;
 
+
+
+
+
 namespace BikeVT.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -29,31 +33,86 @@ namespace BikeVT.Views
         async void OnGetWeatherButtonClicked(object sender, EventArgs e)
         {
             //TODO: probably want to check for location here if we aren't making sure it isn't enabled elsewhere
-                WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+            string req_uri = await GenerateRequestUri(Constants.OpenWeatherMapEndpoint);
+                WeatherData weatherData = await _restService.GetWeatherData(req_uri);
                 BindingContext = weatherData;
         }
 
-        string GenerateRequestUri(string endpoint)
+        async Task<string> GenerateRequestUri(string endpoint)
         {
-            //TODO: check or otherwise verify that location services are enabled
-            var c_locator = CrossGeolocator.Current;
-            if (c_locator.IsGeolocationAvailable && c_locator.IsGeolocationEnabled)
-            {
-                var test_loc = Task.Run(() => c_locator.GetPositionAsync(TimeSpan.FromSeconds(.5))).Result;
 
-                string requestUri = endpoint;
-                requestUri += "?lat=" + test_loc.Latitude;
-                requestUri += "&lon=" + test_loc.Longitude;
-                requestUri += "&units=imperial"; // or units=metric
-                requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
-                return requestUri;
+            //https://github.com/jamesmontemagno/PermissionsPlugin
+            try
+            {
+                Plugin.Permissions.Abstractions.PermissionStatus device_status = await CrossPermissions.Current.CheckPermissionStatusAsync<LocationPermission>();
+
+                if (device_status != Plugin.Permissions.Abstractions.PermissionStatus.Granted)
+                {
+                    await DisplayAlert("Location required", "Location permissions are required to to look up local weather.", "OK");
+
+
+                    // if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+                    // {
+                    //     //Haven't looked into what makes this if-statement true. 
+                    //     // We shouldn't need this since we've already displayed an alert explaining why we need permissions
+                    //     await DisplayAlert("Need location", "Gunna need that location", "OK");
+                    // }
+
+                    //Open prompt to settings:
+                    await Utils.CheckPermissions(new LocationPermission());
+
+                    //The next lines dont wait for you to return from the Settings app. It'll think you denied permissions
+                    Console.WriteLine("refreshing");
+                    device_status = await CrossPermissions.Current.RequestPermissionAsync<LocationPermission>();
+                }
+
+                if (device_status == Plugin.Permissions.Abstractions.PermissionStatus.Granted)
+                {
+                    //Query permission
+
+                    var c_locator = CrossGeolocator.Current;
+                    var test_loc = Task.Run(() => c_locator.GetPositionAsync(TimeSpan.FromSeconds(.5))).Result;
+
+                    string requestUri = endpoint;
+                    requestUri += "?lat=" + test_loc.Latitude;
+                    requestUri += "&lon=" + test_loc.Longitude;
+                    requestUri += "&units=imperial"; // or units=metric
+                    requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+                    return requestUri;
+                }
+                else // if (device_status != Plugin.Permissions.Abstractions.PermissionStatus.Unknown)
+                {
+                    //location denied.
+                    // or permission is unknown. 
+
+                    await DisplayAlert("Permission denied", "Location permissions have been denied. " +
+                        "Using Blacksburg as the default location.\n" +
+                        "If you just allowed location, press the 'Get Weather' button again.\n", "OK");
+                    Console.WriteLine("Get weather location Default action");
+
+                    string uri = endpoint;
+                    uri += "?lat=" + "37.229342";
+                    uri += "&lon=" + "-80.413928";
+                    uri += "&units=imperial"; // or units=metric
+                    uri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+                    return uri;
+                }
+
             }
-            string uri = endpoint;
-            uri += "?lat=" + "37.229342";
-            uri += "&lon=" + "-80.413928";
-            uri += "&units=imperial"; // or units=metric
-            uri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
-            return uri;
+            catch (Exception ex)
+            {
+
+                //Something went wrong
+                await DisplayAlert("Error getting location","Something went wrong. Defaulting location to Blacksburg.", "OK");
+                Console.WriteLine("Something went wrong when getting location for weather. Defaulting to Blacksburg.\n" + ex);
+
+                string uri = endpoint;
+                uri += "?lat=" + "37.229342";
+                uri += "&lon=" + "-80.413928";
+                uri += "&units=imperial"; // or units=metric
+                uri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+                return uri;
+            }
         }
     }
 
